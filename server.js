aa1dcf89c905b3c2be97b84b4ee956353704395b
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-05-07 22:02:32 -0400

    adjusted

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  // Original values
-  // app.use(express.static("client/build"));
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
-  });
-}
 // mongodb+srv://<user>:<password>@cluster0-fgs8h.mongodb.net/mongo-crud?retryWrites=true&w=majority
 // mongodb://heroku_dv0v87gv:ldeush022ljoseouqsfia1oh33@ds113942.mlab.com:13942/heroku_dv0v87gv
 
@@ -32,6 +22,18 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 );
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  // Original values
+  // app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
+
 
 // Send every request to the React app
 // Define any API routes before this runs
